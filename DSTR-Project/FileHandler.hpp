66b8b5f67db22f73cd73f4c1a26358a52ba77011
caizhen <zhenchai0000@gmail.com>
2024-04-21 21:53:13 +0800

    update: file handler to test get csv file content

--- DSTR-Project/FileHandler.hpp
+++ DSTR-Project/FileHandler.hpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 void fileHandler() 
 {
-    cout << "File handler";
-    cout << "File handler";
+    // ifstream = read from file
+    // ofstream = write on file
+    // fstream = both read and write file
+    fstream questionFile;
+    string output;
+    int counter = 1;
+
+    cout << "File handler" << endl;
+
+    questionFile.open("docs/dstr-question-M.csv");
+
+    while (getline(questionFile, output))
+    {
+        // test the csv display
+        cout << counter << ". " << output << endl;
+        counter++;
+    }
 }
