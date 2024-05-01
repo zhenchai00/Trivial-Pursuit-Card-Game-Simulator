008b76650d84e57f7cb54d1b9944b2a64e81d5b4
caizhen <zhenchai0000@gmail.com>
2024-05-01 08:53:28 +0800

    update file structure
    move the unwanted csv file to temp folder

--- DSTR-Project/StudentResult.hpp
+++ DSTR-Project/StudentResult.hpp
@@ -126,7 +126,7 @@ public:
     }
 
     // get student nodes by student id which traverse whole linked list from first to last
-    StudentResultNode *getNodeById (int studentId)
+    StudentResultNode *getNodeById (int studentId, int totalStudent)
     {
         StudentResultNode *current = head;
         while (current != nullptr) // O(n)
